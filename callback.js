24d89516ea8083fff2529fd6e681355742a0c8db
Awasthi95 <shouryaawasthi45@gmail.com>
2025-04-11 15:24:13 +0530

    add file

---
+++ callback.js
@@ -0,0 +1,26 @@
+// just to prevent then function calling of display in side the cal func even not to call two function to display the result 
+function display(res){
+    console.log('Addition of two number is ${res}');
+}
+function calculation(a,b,cb){
+    let sum=a+b;
+    cb(sum);
+
+}
+calculation(4,3,display);
+
+function first(){
+    console.log("print first");
+}
+function second(){
+    console.log("print second");
+}
+
+function third(){
+    console.log("print third");
+
+}
+first();
+setTimeout(second,1000);
+third();
+
